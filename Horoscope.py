22ed304f35517d7e4c27afc42f3ad4a83382da6c
Sylouan <sylouan.corfa@epsi.fr>
2019-02-19 16:50:40 +0100

    Added AstroYogi + user input

--- Horoscope.py
+++ Horoscope.py
@@ -2,6 +2,7 @@ from lxml import html
 import nltk
 import urllib
 import random
+import re, string
 import requests
 from datetime import datetime, timezone
 from bs4 import BeautifulSoup
@@ -9,6 +10,12 @@ from bs4 import BeautifulSoup
 
 class Horoscope:
 
+    @staticmethod
+    def getDateDay():
+        date = datetime.now().strftime('%A')
+        return date
+
+    
     @staticmethod
     def getDate():
         date = datetime.now().strftime('%A-%d-%B')
@@ -22,7 +29,29 @@ class Horoscope:
         horoscope = str(tree.xpath(
             "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
         
-        horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "").replace("[\'", "").replace("\']", "") 
+        horoscope = Horoscope.replaceText(horoscope)
+        return horoscope
+
+
+    @staticmethod
+    def replaceText(text):
+
+        text = text.replace("\\n", "").replace("  ", "").replace("[", "").replace("]", "").replace("'", "").replace("�", "").replace("\"", "")
+        return text
+        
+
+
+    @staticmethod
+    def getAstroyogi(signe):
+        
+        url = "https://www.astroyogi.com/horoscopes/daily/" + signe + "-free-horoscope.aspx"
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        horoscope = str(tree.xpath(
+            "//*[@id=\"today1\"]/p[1]/span/text()"))
+        
+        horoscope = Horoscope.replaceText(horoscope)
+
         return horoscope
 
 
@@ -32,16 +61,16 @@ class Horoscope:
         response = requests.get(url)
         tree = html.fromstring(response.content)
         horoscope = str(tree.xpath(
-            "//section[@class='horoscope']/div[3]/div[1]/p[1]/text()"
+            "//section[@class=\"horoscope\"]/div[3]/div[1]/p[1]/text()"
             ))
 
-        horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "").replace("[\'","").replace("\']","")
-        
+        horoscope = Horoscope.replaceText(horoscope)
+
         return horoscope
 
     @staticmethod
     def tokenizeFullHoroscope(signe):
-        fullHoroscope = Horoscope.getGanesha(signe) + Horoscope.getAstrologyDotCom(signe)
+        fullHoroscope = Horoscope.getGanesha(signe) + Horoscope.getAstrologyDotCom(signe) + Horoscope.getAstroyogi(signe)
         token = nltk.sent_tokenize(fullHoroscope)
         return token
 
@@ -53,10 +82,39 @@ class Horoscope:
         random.shuffle(intList)
         newHoroscope = ""
 
-        for x in range(int(len(sentences)/2)):
+        for x in range(4):
             newHoroscope += sentences[intList[x]]
+        
+        newHoroscope = Horoscope.replaceText(newHoroscope)
         print(newHoroscope)
 
-Horoscope.getRandomHoroscope("virgo")
+
+
+array = [
+    'aries',
+    'pisces',
+    'leo',
+    'cancer',
+    'taurus',
+    'gemini',
+    'virgo',
+    'libra',
+    'scorpio',
+    'sagittarius',
+    'capricorn',
+    'aquarius'
+]
+x = 0
+
+for sign in array:
+    print(str(x) + " - " + sign)
+    x += 1
+
+signe = input("What is your star sign ?\n")
+type(signe)
+
+Horoscope.getRandomHoroscope(array[int(signe)])
+
+
 
     
